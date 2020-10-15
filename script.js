e22a98e83ba693cef9d82f581f7c04f35b747a26
miguelcoria94 <miguelcoria94@gmail.com>
2020-10-15 18:10:09 -0500

    fixed bug with DOM

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
-const count = 10;
-const apiKey = 'gDW2iUxo4HblHeWneR5hlt9KJH73CaMKQPxRFxrqfUQ';
-const apiURL = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
-
-
+document.addEventListener("DOMContentLoaded", (event) =>{
+    
+    const count = 10;
+    const apiKey = 'gDW2iUxo4HblHeWneR5hlt9KJH73CaMKQPxRFxrqfUQ';
+    const apiURL = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
+    
+    async function getPhotos() {
+        try {
+            const response = await fetch(apiURL);
+            const data = await response.json();
+            console.log(data)
+        } catch (error) {
+    
+        }
+    }
+    
+    getPhotos()
+})
 
